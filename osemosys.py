833c69920bf9be2f15d9d0bf277d9d581c2e49cb
Will Usher <wusher@kth.se>
2020-06-10 13:57:38 +0200

    Tidied up pre-amble

--- osemosys.py
+++ osemosys.py
@@ -1,35 +1,28 @@
-# OSeMOSYS_2015_08_27
-#
-# Open Source energy MOdeling SYStem
-#
-# Main changes to previous version OSeMOSYS_2013_05_10
-# 		- Removed the parameter TechWithCapacityNeededToMeetPeakTS from constraint CAa4_Constraint_Capacity
-# 		- Fixed a bug related to using CapacityOfOneTechnologyUnit in constraint CAa5_TotalNewCapacity
-# 		- Fixed a bug in the storage equations which caused an error if more than one day type was used
-# 		- DiscountRate is no longer technology-specific. Therefore, DiscountRateStorage is now replaced by DiscountRate.
-#
-# ============================================================================
-#
-#    Copyright [2010-2015] [OSeMOSYS Forum steering committee see: www.osemosys.org]
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-# ============================================================================
-#
-#              			#########################################
-######################			Model Definition				#############
-#              			#########################################
-#
-#
+"""
+
+OSeMOSYS: Open Source energy MOdeling SYStem
+
+============================================================================
+
+  Copyright [2010-2015] [OSeMOSYS Forum steering committee see: www.osemosys.org]
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+
+             			#########################################
+#####################			Model Definition				#############
+             			#########################################
+
 from __future__ import division
 from pyomo.environ import *
 from pyomo.core import *
